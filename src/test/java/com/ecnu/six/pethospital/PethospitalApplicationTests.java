5b84f80fb5be5c4cb4a679c46d765fc111ae860f
onion <969023014@qq.com>
2021-03-15 09:38:31 +0800

    basic structure

---
+++ src/test/java/com/ecnu/six/pethospital/PethospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecnu.six.pethospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PethospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
